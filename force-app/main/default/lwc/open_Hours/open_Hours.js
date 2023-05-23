f4dfcf842d6b40e19d421081e870128752cc08c3
Jakub Szewczyk <240810@edu.p.lodz.pl>
2023-05-23 11:56:57 +0200

    displaying whole week hours

--- force-app/main/default/lwc/open_Hours/open_Hours.js
+++ force-app/main/default/lwc/open_Hours/open_Hours.js
@@ -1,14 +1,15 @@
 import {LightningElement, wire, api} from 'lwc';
 import showHours from '@salesforce/apex/HoursController.showHours';
-
 export default class Open_Hours extends LightningElement {
     @api recordId;
     hour;
+    weekendHour='';
 
     @wire(showHours, {shelterID: '$recordId'})
     wiredAccount({ error, data }) {
         if (data) {
-            this.hour = data;
+            this.hour = data[0];
+            this.weekendHour = data[1];
         } else if (error) {
             console.error(error);
         }

a4d7cf9a7d0592bdfa16a397f0276fda39c6023f
Jakub Szewczyk <240810@edu.p.lodz.pl>
2023-06-27 21:36:24 +0200

    einstein prediction

--- force-app/main/default/lwc/animalSearcher/animalSearcher.js
+++ force-app/main/default/lwc/animalSearcher/animalSearcher.js
@@ -1,5 +1,7 @@
 import { LightningElement, track, wire } from 'lwc';
 import getAllAnimalObjects from '@salesforce/apex/SandboxAuthorization.getAnimalsObjects';
+import getBreedFromPhoto from '@salesforce/apex/EinsteinTokenGenerator.uploadFileToApex';
+
 export default class AnimalSearcher extends LightningElement {
     @track shelter = 'any';
     @track breed = 'any';
@@ -52,16 +54,26 @@ export default class AnimalSearcher extends LightningElement {
     }
 
     handleFileChange(event) {
-        this.imageFile = event.target.files[0];
-        // TODO: Use this imageFile with image recognition API to get the breed
+        const file = event.target.files[0];
+        this.uploadFile(file);
     }
 
-    
+    uploadFile(file) {
+        const reader = new FileReader();
 
+        reader.onload = () => {
+            const base64 = reader.result.split(',')[1];
 
-    handleSearch() {
-        // TODO: Use the parameters to perform search in Salesforce or through external API
-        // This should update the `animals` property with the search results
+            getBreedFromPhoto({ base64: base64 })
+                .then(result => {
+                    console.log('result: '+result)
+                })
+                .catch(error => {
+                    console.log(error)
+                });
+        };
+
+        reader.readAsDataURL(file);
     }
 
     getOptions(field) {
